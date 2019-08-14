5bf68dd51b8bf380f8b317554ba0aacadea28059
nhavua <vuvannhan.hut@gmail.com>
2019-08-14 22:38:01 +0700

    3.2 Create entity: Category

---
+++ src/ProductBundle/Entity/Category.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace ProductBundle\Entity;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     */
+    private $creationDate;
+
+    /**
+     * @var \DateTime
+     */
+    private $editionDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set creationDate
+     *
+     * @param \DateTime $creationDate
+     *
+     * @return Category
+     */
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get creationDate
+     *
+     * @return \DateTime
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * Set editionDate
+     *
+     * @param \DateTime $editionDate
+     *
+     * @return Category
+     */
+    public function setEditionDate($editionDate)
+    {
+        $this->editionDate = $editionDate;
+
+        return $this;
+    }
+
+    /**
+     * Get editionDate
+     *
+     * @return \DateTime
+     */
+    public function getEditionDate()
+    {
+        return $this->editionDate;
+    }
+}
+
