5f2167ff88c078a66f7905546408e782c48c1560
nhavua <vuvannhan.hut@gmail.com>
2019-08-15 00:24:07 +0700

    Remove creationDate and editionDate on form

--- src/ProductBundle/Form/CategoryType.php
+++ src/ProductBundle/Form/CategoryType.php
@@ -4,6 +4,8 @@ namespace ProductBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategoryType extends AbstractType
@@ -13,7 +15,25 @@ class CategoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('creationDate')->add('editionDate');
+        $builder->add('name');
+
+        $builder->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $category = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback functions!
+                $current = date_create();
+                if (null === $category->getId()) {
+                    $category->setCreationDate($current);
+                }
+
+                $category->setEditionDate($current);
+            }
+        );
     }
 
     /**
--- src/ProductBundle/Form/ProductType.php
+++ src/ProductBundle/Form/ProductType.php
@@ -4,6 +4,8 @@ namespace ProductBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -13,7 +15,29 @@ class ProductType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('number')->add('name')->add('price')->add('creationDate')->add('editionDate')->add('category');
+        $builder
+            ->add('number')
+            ->add('name')
+            ->add('price')
+            ->add('category');
+
+        $builder->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $product = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback functions!
+                $current = date_create();
+                if (null === $product->getId()) {
+                    $product->setCreationDate($current);
+                }
+
+                $product->setEditionDate($current);
+            }
+        );
     }
 
     /**
