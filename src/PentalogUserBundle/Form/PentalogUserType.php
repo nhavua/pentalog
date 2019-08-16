4fdd054a868744415978543f6c89bd08dfcfd738
nhavua <vuvannhan.hut@gmail.com>
2019-08-16 07:24:16 +0700

    Allow create user with roll ADMIN/USER

--- src/PentalogUserBundle/Form/PentalogUserType.php
+++ src/PentalogUserBundle/Form/PentalogUserType.php
@@ -3,7 +3,10 @@
 namespace PentalogUserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PentalogUserType extends AbstractType
@@ -13,8 +16,42 @@ class PentalogUserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username')->add('password')->add('salt')->add('role')->add('creationDate')->add('editionDate');
-    }/**
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('salt')
+            ->add('role', ChoiceType::class, array(
+                    'attr' => array('class' => 'form-control',
+                        'style' => 'margin:5px 0;'),
+                    'choices' => array(
+                        'ROLE_ADMIN' => 'ROLE_ADMIN',
+                        'ROLE_USER' => 'ROLE_USER',
+                    ),
+                    'multiple' => false,
+                    'required' => true,
+                )
+            );
+
+        $builder->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $pentalogUser = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback functions!
+                $current = date_create();
+                if (null === $pentalogUser->getId()) {
+                    $pentalogUser->setCreationDate($current);
+                }
+
+                $pentalogUser->setEditionDate($current);
+            }
+        );
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
